33f135eedc083a7c88afabf432dfc3cf1c979117
Cezary Suchorski <cezary.suchorski@gmail.com>
2022-08-18 11:20:30 +0200

    Added review resource for users and admins in a separate scope /admin

--- lib/hello_web/router.ex
+++ lib/hello_web/router.ex
@@ -17,16 +17,22 @@ defmodule HelloWeb.Router do
 
   scope "/", HelloWeb do
     pipe_through :browser
-
     get "/", PageController, :index
 
     resources "/users", UserController do
       resources "/posts", PostController
     end
 
+    resources "/reviews", ReviewController
 
     get "/hello", HelloController, :index
     get "/hello/:messenger", HelloController, :show
+  end
+
+  scope "/admin", HelloWeb.Admin, as: :admin do
+    pipe_through :browser
+
+    resources "/reviews", ReviewController
   end
 
   # Other scopes may use custom stacks.
