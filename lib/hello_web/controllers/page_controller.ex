977b56685375006e835fc5a6746438b25d42f39a
Cezary Suchorski <cezary.suchorski@gmail.com>
2022-08-22 17:54:50 +0200

    Added some flash messages to PageController index action and retrieved them in app.html.heex

--- lib/hello_web/controllers/page_controller.ex
+++ lib/hello_web/controllers/page_controller.ex
@@ -2,7 +2,11 @@ defmodule HelloWeb.PageController do
   use HelloWeb, :controller
 
   def index(conn, _params) do
-    redirect(conn, to: Routes.page_path(conn, :redirect_test))
+    conn
+    |> put_flash(:info, "Welcome to Phoenix, from flash info!")
+    |> put_flash(:error, "Let's pretend we have an error.")
+    |> put_flash(:test, "just a test of flash messages")
+    |> redirect(to: Routes.page_path(conn, :redirect_test))
   end
 
   def redirect_test(conn, _params) do
